2a6d09b607c18a884d71a41119d00b1d3ec89740
@vaneca06-is <vaneca06@gmail.com>
2025-10-14 22:02:30 -0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h2>Tareas completadas: {{ contadorCompletadas }} / {{ tasks.length }}</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>TASK</th>
+          <th>STATUS</th>
+          <th>ACTION</th>
+        </tr>
+      </thead>
+      <tbody>
+        <TaskItem
+          v-for="task in tasks"
+          :key="task.id"
+          :task="task"
+          @task-completed="manageStatus"
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import TaskItem from './components/TaskItem.vue'
+import { computed, ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const tasks = ref([
+  { id: 1, name: "Estudiar Vue.js", done: false },
+  { id: 2, name: "Practicar props", done: false },
+  { id: 3, name: "Hacer el ejercicio", done: false },
+]);
+
+const manageStatus = (taskId) => {
+  const taskToComplete = tasks.value.find(t => t.id == taskId);
+  if (taskToComplete) {
+    taskToComplete.done = true;
   }
-}
+};
+
+const contadorCompletadas = computed(() => {
+  return tasks.value.filter(t => t.done).length;
+});
 </script>
 
 <style>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <tr>
+    <td>{{ task.id }}</td>
+    <td :style="{ textDecoration: task.done ? 'line-through' : 'none' }">
+      {{ task.name }}
+    </td>
+    <td>{{ task.done }}</td>
+    <td>
+      <span v-if="task.done">✅</span>
+      <button
+        v-if="!task.done"
+        @click="emitCompletion"
+      >
+        Completar
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+  task: {
+    type: Object,
+    required: false,
+  },
+});
+
+const emit = defineEmits(['task-completed']);
+
+const emitCompletion = () => {
+  emit('task-completed', props.task.id);
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
